48bdad0fe8690d76c2c0953482274923f361d361
ethanbokame <ethaniraqui@gmail.com>
2026-04-19 16:50:58 +0000

    update config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Config:
     # MySQL root sans mot de passe
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "mysql+pymysql://root:1234@localhost:3307/medical_ia_agent"
+        # "mysql+pymysql://root:1234@localhost:3307/medical_ia_agent"
+        "mysql+pymysql://root:n5z94zvKXiPEzSIumBPiqlelxGexnAfz@dpg-d7ifghnaqgkc739snsig-a:5432/medical_ia_agent_backend"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
