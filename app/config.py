2c31a5ab2145c27e1a7f4b219883552a2cd6ce9c
ethanbokame <ethaniraqui@gmail.com>
2026-04-19 22:21:10 +0000

    add aiven

--- app/config.py
+++ app/config.py
@@ -9,6 +9,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
         # "mysql+pymysql://root:1234@localhost:3307/medical_ia_agent"
-        "mysql+pymysql://avnadmin:{PWD_AIVEN}@mysql-136fdcf2-ethaniraqui-3e24.e.aivencloud.com:14567/defaultdb"
+        "mysql+pymysql://avnadmin:AVNS_8AbLwjJlrUModRwnl8E@mysql-136fdcf2-ethaniraqui-3e24.e.aivencloud.com:14567/defaultdb"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
